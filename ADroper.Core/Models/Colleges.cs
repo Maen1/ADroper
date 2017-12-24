e585652156334c33f981acfa78e4811a62e08cf0
Ayman Mohammed <ayman-2015@outlook.com>
2017-12-24 16:31:51 +0800

    Initial Commit

---
+++ ADroper.Core/Models/Colleges.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADroper.Core.Models
+{
+    public class Colleges
+    {
+        public const string LAWS = "LAWS";
+        public const string ECONS = "ENMS";
+        public const string IRKHS = "IRKHS";
+        public const string ENGIN = "ENGIN";
+        public const string ARCHITECTURE = "AED";
+        public const string EDUCATION = "EDUC";
+        public const string ICT = "KICT";
+        public const string SCIENCE = "KOS";
+        public const string CELPAD = "CFL";
+        public const string COCU = "CCAC";
+        public const string KLM = "KLM";
+        public const string ISTAC = "ISTAC";
+        public const string MEDICINE = "MEDIC";
+        public const string PHARMACY = "PHARM";
+        public const string NURSING = "NURS";
+        public const string ALLIED_HEALTH_SCIENCES = "KAHS";
+        public const string DENTISTRY = "DENT";
+        public const string BRIDGING_PROGRAMME = "BRIDG";
+        public const string ISLAMIC_BANKING_AND_FINANCE = "IIBF";
+        public const string INTERNATIONAL_INSTITUTE_FOR_HALAL_RESEARCH_AND_TRAINING = "IHART";
+    }
+}
---
+++ ADroper.Core/Models/Degrees.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADroper.Core.Models
+{
+    public class Degrees
+    {
+        public const string UNDERGRADUATE = "<";
+
+        public const string POSTGRADUATE = ">=";
+    }
+}
